b12ebcefabda6248d1142403cc40f9576b27f2c6
WladMadi <wlad@madi.space>
2022-07-16 11:16:46 +0300

    Initial Commit

--- GitTest/ViewController.swift
+++ GitTest/ViewController.swift
@@ -8,10 +8,12 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let button = UIButton()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        button.backgroundColor = .blue
     }
 
 
